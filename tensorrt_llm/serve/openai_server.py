79418ae8912164a0b4916fec7f8813024e99ba67
Talor Abramovich <talor19@gmail.com>
2025-06-29 15:46:30 +0300

    [TRTLLM-5831][feat] Add LoRA support for pytorch backend in trtllm-serve (#5376)
    Signed-off-by: Talor Abramovich <talora@nvidia.com>

--- tensorrt_llm/serve/openai_server.py
+++ tensorrt_llm/serve/openai_server.py
@@ -294,6 +294,7 @@ class OpenAIServer:
                 sampling_params=sampling_params,
                 _postproc_params=postproc_params if self.postproc_worker_enabled else None,
                 streaming=request.stream,
+                lora_request=request.lora_request,
                 disaggregated_params=disaggregated_params
             )
             asyncio.create_task(self.await_disconnected(raw_request, promise))
@@ -415,6 +416,7 @@ class OpenAIServer:
                     sampling_params=sampling_params,
                     _postproc_params=postproc_params,
                     streaming=request.stream,
+                    lora_request=request.lora_request,
                     disaggregated_params=disaggregated_params
                 )
                 asyncio.create_task(self.await_disconnected(raw_request, promise))
